787e7624418c0b34c1a43c91a20a685bb5e9adb3
soyoung125 <sypark041754@gmail.com>
2023-08-30 18:02:28 +0900

    chore: 초기 설정 - 프로젝트 설치 - gitgub pages 자동배포 설정

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base:'/turtle-insight/' // 이 부분을 수정
+})
