1c4b94b344cb1865c172adfb0f00a1607f64f788
soyoung125 <sypark041754@gmail.com>
2023-09-07 22:27:21 +0900

    feat: 로고 클릭 시 메인 화면으로 넘어가도록 수정

--- src/components/common/Topbar.tsx
+++ src/components/common/Topbar.tsx
@@ -3,7 +3,7 @@ import logo from '../../assets/logo_white.png'
 function Topbar() {
     return (
         <div>
-            <a href="" target="_blank">
+            <a href="/" target="_blank">
                 <img src={logo} className="logo" alt="turtleinsight logo" />
             </a>
         </div>
