b2745257b582af3af0669cd2840c240c3719e214
soyoung125 <sypark041754@gmail.com>
2023-09-13 16:53:23 +0900

    feat: 화면에 질문 표시하기

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+import { QUESTIONS } from "../../domain/constants/questions";
+
 function Test() {
+    const question_box = QUESTIONS.map((q, idx) => (
+        <div key={idx}>
+            <div>
+                <div>Q{idx + 1}</div>
+                <button>뒤로가기</button>
+            </div>
+            <div>{q.question}</div>
+            <br />
+            {q.options.map((option) => <div key={Math.random()} onClick={() => setStep(step+1)}>{option.answer}</div>)}
+        </div>
+    ))
+    const [step, setStep] = useState(0);
+    
     return (
-        <>test</>
+        <>
+            {question_box[step]}
+
+            <div>range: {step/20*100}%</div>
+        </>
     );
 }
 
