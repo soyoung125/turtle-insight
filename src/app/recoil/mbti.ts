fdbc3ec70606f11e090f9e068a663df2b3c18c40
soyoung125 <sypark041754@gmail.com>
2023-09-15 00:26:35 +0900

    feat: recoil-persist 적용 - 새로고침 시 데이터 초기화 방지

--- src/app/recoil/mbti.ts
+++ src/app/recoil/mbti.ts
@@ -1,8 +1,12 @@
 import {
     atom,
 } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 export const mbtiState = atom({
     key: 'mbti',
     default: '',
+    effects_UNSTABLE: [persistAtom],
 });
