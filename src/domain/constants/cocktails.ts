8a386b99b6fb0794e0616778a0d6089bbe3510c5
soyoung125 <sypark041754@gmail.com>
2023-09-17 23:28:42 +0900

    feat: mbti에 맞는 칵테일 이미지 띄우기

---
+++ src/domain/constants/cocktails.ts
@@ -0,0 +1,40 @@
+import sex_on_the_beach from '../../assets/images/cocktails/섹온비.gif'
+import pousse_cafe from '../../assets/images/cocktails/파우스트.gif'
+import espresso_martini from '../../assets/images/cocktails/에스프래소마티니.gif'
+import aqua_vitae from '../../assets/images/cocktails/옥보단.gif'
+import lime_mojito from '../../assets/images/cocktails/라임모히또.gif'
+import jungle_bird from '../../assets/images/cocktails/준벅.gif'
+import long_island_iced_tea from '../../assets/images/cocktails/롱아일랜드.gif'
+import midori_sour from '../../assets/images/cocktails/미도리샤워.gif'
+import cosmopolitan from '../../assets/images/cocktails/코스모폴리탄.gif'
+import jagerbomb from '../../assets/images/cocktails/예거밤.gif'
+import peach_crush from '../../assets/images/cocktails/피치크러쉬.gif'
+import blue_hqwaii from '../../assets/images/cocktails/블루하와이.gif'
+import jack_and_coke from '../../assets/images/cocktails/잭콕.gif'
+import gin_and_tonic from '../../assets/images/cocktails/진토닉.gif'
+import tequila_sunrise from '../../assets/images/cocktails/데낄라선라이즈.gif'
+import margarita from '../../assets/images/cocktails/마가리타.gif'
+
+const COCKTAILS: {[key: string]: string} = {
+    ENFP: sex_on_the_beach,
+    ESTJ: pousse_cafe,
+    INTJ: espresso_martini,
+    INPT: aqua_vitae,
+    ISTP: lime_mojito,
+    ESFJ: jungle_bird,
+    ISFJ: long_island_iced_tea,
+    ESTP: midori_sour,
+    ENTP: cosmopolitan,
+    ESFP: jagerbomb,
+    INFP: peach_crush,
+    ENFJ: blue_hqwaii,
+    ISTJ: jack_and_coke,
+    ENTJ: gin_and_tonic,
+    ISFP: tequila_sunrise,
+    INFJ: margarita,
+}
+
+export default null;
+export {
+    COCKTAILS, 
+}
--- src/pages/Result/index.tsx
+++ src/pages/Result/index.tsx
@@ -5,6 +5,7 @@ import {
 import { testResultState } from '../../app/recoil/test';
 import { mbtiState } from '../../app/recoil/mbti';
 import '../../style/Result.css';
+import { COCKTAILS } from '../../domain/constants/cocktails';
 
 function Result() {
     const [isLoading, setIsLoading] = useState(true);
@@ -22,34 +23,36 @@ function Result() {
     return (
         <>
             {isLoading
-            ? <div>isloading</div>
-            : <div>
-                <div className="menu">BEST<br />MENU</div>
-                <div>cocktail img</div>
-
-                <div className='paper'>
-                    <div className='cocktail_name'>cocktail name</div>
-                    <div className='tag'><div className='tag_value'>tag</div></div>
-                    <div className='description'>content</div>
-                </div>
-                
-                <div className='paper description'>
-                    <div>content</div>
-                </div>
-
-                <div className='recommendation'>
-                    <div>
-                        cocktail img
-                        <div>cocktail name</div>
+                ? <div>isloading</div>
+                : <div>
+                    <div className='menu_top'>
+                        <img src={COCKTAILS[mbti]} className="main_cocktail" alt="turtleinsight logo" />
+                        <div className="menu">BEST<br />MENU</div>
                     </div>
-                    <div>
-                        cocktail img
-                        <div>cocktail name</div>
+
+                    <div className='paper cocktail_info'>
+                        <div className='cocktail_name'>cocktail name</div>
+                        <div className='tag'><div className='tag_value'>tag</div></div>
+                        <div className='description'>content</div>
                     </div>
-                </div>
-            </div>}
+
+                    <div className='paper description'>
+                        <div>content</div>
+                    </div>
+
+                    <div className='recommendation'>
+                        <div>
+                            cocktail img
+                            <div>cocktail name</div>
+                        </div>
+                        <div>
+                            cocktail img
+                            <div>cocktail name</div>
+                        </div>
+                    </div>
+                </div>}
         </>
     );
 }
 
- export default Result;
+export default Result;
