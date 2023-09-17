9e1bf6f1c6a2223f33f9433a2d8685a2618e9433
soyoung125 <sypark041754@gmail.com>
2023-09-17 22:01:18 +0900

    feat: 결과 페이지 컴포넌트 배치

--- src/pages/Result/index.tsx
+++ src/pages/Result/index.tsx
@@ -4,6 +4,7 @@ import {
 } from 'recoil';
 import { testResultState } from '../../app/recoil/test';
 import { mbtiState } from '../../app/recoil/mbti';
+import '../../style/Result.css';
 
 function Result() {
     const [isLoading, setIsLoading] = useState(true);
@@ -20,7 +21,33 @@ function Result() {
 
     return (
         <>
-            {isLoading ? <div>isloading</div> : <div>{mbti}</div>}
+            {isLoading
+            ? <div>isloading</div>
+            : <div>
+                <div className="menu">BEST<br />MENU</div>
+                <div>cocktail img</div>
+
+                <div className='paper'>
+                    <div className='cocktail_name'>cocktail name</div>
+                    <div className='tag'><div className='tag_value'>tag</div></div>
+                    <div className='description'>content</div>
+                </div>
+                
+                <div className='paper description'>
+                    <div>content</div>
+                </div>
+
+                <div className='recommendation'>
+                    <div>
+                        cocktail img
+                        <div>cocktail name</div>
+                    </div>
+                    <div>
+                        cocktail img
+                        <div>cocktail name</div>
+                    </div>
+                </div>
+            </div>}
         </>
     );
 }
