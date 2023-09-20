cd3fd96068c2607aeedd92db68e61d4c2df77e91
soyoung park <77377583+soyoung125@users.noreply.github.com>
2023-09-20 21:30:15 +0900

    feat: 메인 화면 디자인 수정 (#31)
    * feat: 시작 화면 배경 이미지 변경
    
    * feat: 메인 페이지 디자인 수정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,24 @@
-import cocktail from '../../assets/images/cocktail_white.png'
 import '../../App.css'
 import { useNavigate } from 'react-router-dom';
 import PATH from '../../domain/constants/path';
+import { COCKTAILS } from '../../domain/constants/cocktails';
 
 function Home() {
     const navigate = useNavigate();
 
     return (
         <>
-            <div className="roundedCard">
+            <div className="blue_text center">
                 <div className="title">MBTI 밸런스 게임<div>-칵테일편-</div></div>
-                <div>
-                    <img src={cocktail} className="logo_cocktail" alt="cocktail logo" />
+                <img src={COCKTAILS.default} className="logo_cocktail" alt="cocktail logo" />
+
+                <div className='start_area'>
+                    <div className='sub_title'>나와 어울리는 칵테일을 찾아보세요.</div>
+                    <button className="start_button" onClick={() => navigate(PATH.test)}>start</button>
                 </div>
-                <button className="start_button" onClick={() => navigate(PATH.test)}>start</button>
             </div>
-            <br />
             <button className="menu_button" onClick={() => navigate(PATH.menu)}>
-                칵테일MENU 보러가기🧾
+                다른 칵테일 메뉴 보러가기
             </button>
         </>
     );
