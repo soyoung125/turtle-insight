071d74a22f5d3b7288c0296597335168bcf515df
soyoung125 <sypark041754@gmail.com>
2023-09-25 01:29:29 +0900

    feat: 이미지 다운로드 기능 구현

---
+++ src/domain/constants/results.ts
@@ -0,0 +1,38 @@
+import ENFJ from '/assets/images/results/ENFJ.jpg';
+import ENFP from '/assets/images/results/ENFP.jpg';
+import ENTJ from '/assets/images/results/ENTJ.jpg';
+import ENTP from '/assets/images/results/ENTP.jpg';
+import ESFJ from '/assets/images/results/ESFJ.jpg';
+import ESFP from '/assets/images/results/ESFP.jpg';
+import ESTJ from '/assets/images/results/ESTJ.jpg';
+import ESTP from '/assets/images/results/ESTP.jpg';
+import INFJ from '/assets/images/results/INFJ.jpg';
+import INFP from '/assets/images/results/INFP.jpg';
+import INTJ from '/assets/images/results/INTJ.jpg';
+import INTP from '/assets/images/results/INTP.jpg';
+import ISFJ from '/assets/images/results/ISFJ.jpg';
+import ISFP from '/assets/images/results/ISFP.jpg';
+import ISTJ from '/assets/images/results/ISTJ.jpg';
+import ISTP from '/assets/images/results/ISTP.jpg';
+
+const RESULTS: { [key: string]: string } = {
+    ENFP: ENFP,
+    ESTJ: ESTJ,
+    INTJ: INTJ,
+    INTP: INTP,
+    ISTP: ISTP,
+    ESFJ: ESFJ,
+    ISFJ: ISFJ,
+    ESTP: ESTP,
+    ENTP: ENTP,
+    ESFP: ESFP,
+    INFP: INFP,
+    ENFJ: ENFJ,
+    ISTJ: ISTJ,
+    ENTJ: ENTJ,
+    ISFP: ISFP,
+    INFJ: INFJ,
+}
+
+export default null;
+export { RESULTS };
