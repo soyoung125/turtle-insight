54236667f71ba57e3247428b41e17d49ddde4547
soyoung125 <sypark041754@gmail.com>
2023-09-27 01:18:01 +0900

    feat: 페이지 하단 kakao 광고 추가

--- src/components/layouts/HomeLayout.tsx
+++ src/components/layouts/HomeLayout.tsx
@@ -2,20 +2,24 @@ import { createBrowserRouter, Outlet, useLocation } from "react-router-dom";
 import Topbar from "../common/Topbar";
 import '../../style/Home.css'
 import { COCKTAILS } from "../../domain/constants/cocktails";
+import KakaoAdFit from "../common/KakaoAdfit";
 
 function HomeLayout() {
     const location = useLocation();
 
     return (
-        <div className={location.pathname === '/' ? 'start_background' : 'pink_background'}>
-            <Topbar />
-            <article className="loading">
-                {Object.keys(COCKTAILS).map((c) => <img src={COCKTAILS[c]} />)}
-            </article>
-            <div className="container">
-                <Outlet />
+        <>
+            <div className={location.pathname === '/' ? 'start_background' : 'pink_background'}>
+                <Topbar />
+                <article className="loading">
+                    {Object.keys(COCKTAILS).map((c) => <img src={COCKTAILS[c]} />)}
+                </article>
+                <div className="container">
+                    <Outlet />
+                </div>
             </div>
-        </div>
+            <KakaoAdFit />
+        </>
     );
 }
 
